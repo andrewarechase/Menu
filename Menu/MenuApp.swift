a75f7e38469c4b09192ba201f627adac599884e2
Andreware <achase047@proton.me>
2024-09-24 14:33:26 -0400

    Initial Commit

---
+++ Menu/MenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuApp.swift
+//  Menu
+//
+//  Created by Andrew Chase on 8/20/24.
+//
+
+import SwiftUI
+
+@main
+struct MenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MenuView()
+        }
+    }
+}
---
+++ Menu/MenuView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  Menu
+//
+//  Created by Andrew Chase on 8/20/24.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    
+    @State var menuItems:[MenuItem] = [MenuItem]()
+    var dataService = DataService()
+    
+    var body: some View {
+     
+        List(menuItems) { item in
+            
+            
+           MenuListRow(item: item)
+            
+            
+        }
+        .listStyle(.plain)
+        .onAppear {
+            //Call for the Data
+            menuItems = dataService.getData()
+            
+        }
+    }
+}
+
+#Preview {
+    MenuView()
+}
